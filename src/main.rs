23550ce1a8fbe000b18b699f335586bf1aff666b
Isamade <52768347+Isamade@users.noreply.github.com>
2026-01-31 21:12:04 +0100

    feat: add cors to server

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use axum::{routing::get, routing::post, Router};
 use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer};
+use axum::http::Method;
 
 mod rotation;
 
 #[tokio::main]
 async fn main() {
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods([Method::GET, Method::POST])
+        .allow_headers(Any);
+
     let app = Router::new()
         .route("/", get(rotation::hello_world));
+        .layer(cors);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Server listening on {}", addr);
